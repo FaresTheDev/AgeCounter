52cedf2d863a94d2f3d429d9d6db4f70eab4852b
Fares Alharbi <99187602+FaresTheDev@users.noreply.github.com>
2023-05-01 17:21:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,16 +14,17 @@ const showDays = document.getElementById("days");
 const dayError = document.getElementById("day-error");
 const monthError = document.getElementById("month-error");
 const yearError = document.getElementById("year-error");
+
 //to prevent defualt
 const form = document.querySelector("form");
 
+//check if the day is valid (1-31)
 function isDayValid(userDay) {
-  //check if the day is valid (1-31)
   if (isNaN(userDay) || userDay < 1 || userDay > 31) {
     dayError.removeAttribute("hidden");
     dayError.textContent = "must be a valid day";
     dayInput.value = "";
-    //add error class to the day input and label
+
     dayInput.classList.add("error");
     dayLabel.classList.add("error");
     return;
@@ -35,13 +36,13 @@ function isDayValid(userDay) {
   }
 }
 
+//check if the month is valid (1-12)
 function isMonthValid(userMonth) {
-  //check if the month is valid (1-12)
   if (isNaN(userMonth) || userMonth < 1 || userMonth > 12) {
     monthError.removeAttribute("hidden");
     monthError.textContent = "must be a valid month";
     monthInput.value = "";
-    //add error class to the month input and label
+
     monthInput.classList.add("error");
     monthLabel.classList.add("error");
     return;
@@ -53,13 +54,13 @@ function isMonthValid(userMonth) {
   }
 }
 
+//check if the year is valid (not in the future) & (not in the past)
 function isYearValid(userYear) {
-  //check if the year is valid (not in the future) & (not in the past)
   if (isNaN(userYear) || userYear < 1900) {
     yearError.removeAttribute("hidden");
     yearError.textContent = "must be a valid year";
     yearInput.value = "";
-    //add error class to the year input and label
+
     yearInput.classList.add("error");
     yearLabel.classList.add("error");
     return;
@@ -102,7 +103,52 @@ form.addEventListener("submit", (event) => {
   const diffMonths = Math.floor((diffMs % 31536000000) / 2592000000); // 1 month = 2592000000 milliseconds
   const diffDays = Math.floor(((diffMs % 31536000000) % 2592000000) / 86400000); // 1 day = 86400000 milliseconds
 
-  showYears.textContent = `${diffYears} years`;
-  showMonths.textContent = `${diffMonths} months`;
-  showDays.textContent = `${diffDays} days`;
+  //animate the days number
+  function countToDayInput(userDayInput) {
+    let count = 0;
+    const increment = Math.ceil(userDayInput / 100); // divide the user input by 100 to increment by 1% of the value each time
+    const interval = setInterval(() => {
+      count += increment;
+      if (count >= userDayInput) {
+        // check if the count has reached the user input value
+        clearInterval(interval);
+        count = userDayInput;
+      }
+      showDays.textContent = `${count} days`;
+    }, 10); // update the display every 10 milliseconds
+  }
+
+  //animate the days number
+  function countToMonthInput(userMonthInput) {
+    let count = 0;
+    const increment = Math.ceil(userMonthInput / 100); // divide the user input by 100 to increment by 1% of the value each time
+    const interval = setInterval(() => {
+      count += increment;
+      if (count >= userMonthInput) {
+        // check if the count has reached the user input value
+        clearInterval(interval);
+        count = userMonthInput;
+      }
+      showMonths.textContent = `${count} months`;
+    }, 10); // update the display every 10 milliseconds
+  }
+
+  //animate the days number
+  function countToYearInput(userYearInput) {
+    let count = 0;
+    const increment = Math.ceil(userYearInput / 100); // divide the user input by 100 to increment by 1% of the value each time
+    const interval = setInterval(() => {
+      count += increment;
+      if (count >= userYearInput) {
+        // check if the count has reached the user input value
+        clearInterval(interval);
+        count = userYearInput;
+      }
+      showYears.textContent = `${count} years`;
+    }, 10); // update the display every 10 milliseconds
+  }
+
+  countToDayInput(diffDays);
+  countToMonthInput(diffMonths);
+  countToYearInput(diffYears);
 });
